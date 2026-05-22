1ea9e88dcb7fb780173def213b985bfac7d98623
Serge <sajghaa@gmail.com>
2026-05-22 14:38:27 +0200

    Updated crud.py

---
+++ python/fastapi/todo-api/src/crud.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_todo(db: Session, todo_id: int):
+    return db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+
+def get_todos(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Todo).offset(skip).limit(limit).all()
+
+def create_todo(db: Session, todo: schemas.TodoCreate):
+    db_todo = models.Todo(title=todo.title, description=todo.description)
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+def update_todo(db: Session, todo_id: int, todo_update: schemas.TodoUpdate):
+    db_todo = get_todo(db, todo_id)
+    if not db_todo:
+        return None
+    for var, value in vars(todo_update).items():
+        if value is not None:
+            setattr(db_todo, var, value)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+def delete_todo(db: Session, todo_id: int):
+    db_todo = get_todo(db, todo_id)
+    if db_todo:
+        db.delete(db_todo)
+        db.commit()
+        return True
+    return False

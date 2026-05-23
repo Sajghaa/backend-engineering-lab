bfb4001b19546308346c8097bc64c9989a093eb4
Serge <sajghaa@gmail.com>
2026-05-23 09:05:41 +0200

    Updated schemas.py

--- python/fastapi/todo-api/src/schemas.py
+++ python/fastapi/todo-api/src/schemas.py
@@ -22,3 +22,27 @@ class TodoResponse(TodoBase):
 
     class Config:
         from_attributes = True
+
+
+# --- User schemas ---
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+    is_active: bool
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+# --- Token schemas ---
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None

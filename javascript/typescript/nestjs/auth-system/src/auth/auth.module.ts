76181bc440101b1637523fa4a43320b42ce17191
Serge <sajghaa@gmail.com>
2026-07-16 21:38:03 +0200

    Updated auth.module.ts

--- javascript/typescript/nestjs/auth-system/src/auth/auth.module.ts
+++ javascript/typescript/nestjs/auth-system/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'fallback-secret',
+      signOptions: { expiresIn: '30m' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}

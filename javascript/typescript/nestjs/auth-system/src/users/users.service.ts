238128f61d92662b8c0293805ac7ef184d49603f
Serge <sajghaa@gmail.com>
2026-07-16 21:36:36 +0200

    Updated users.service.ts

--- javascript/typescript/nestjs/auth-system/src/users/users.service.ts
+++ javascript/typescript/nestjs/auth-system/src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(email: string, username: string, password: string, role = 'user'): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.usersRepository.create({ email, username, hashedPassword, role });
+    return this.usersRepository.save(user);
+  }
+
+  async findByUsername(username: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { username } });
+  }
+  
+  async findById(id: number): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+}

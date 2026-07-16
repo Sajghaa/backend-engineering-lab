7ebc8b102b76e426d45710d82bba1d87099e5a74
Serge <sajghaa@gmail.com>
2026-07-16 21:10:24 +0200

    Updated app.module.ts

--- javascript/typescript/nestjs/auth-system/src/app.module.ts
+++ javascript/typescript/nestjs/auth-system/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'auth.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, 
+    }),
+    AuthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}

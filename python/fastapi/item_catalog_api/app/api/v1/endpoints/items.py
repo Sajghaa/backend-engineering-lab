e680c0dfb57713e08e541db4655299d6b6a87717
Serge <sajghaa@gmail.com>
2026-07-24 22:24:57 +0200

    updating endpints(app/api/v1/endpoints/items.py)

--- python/fastapi/item_catalog_api/app/api/v1/endpoints/items.py
+++ python/fastapi/item_catalog_api/app/api/v1/endpoints/items.py
@@ -1,17 +1,33 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy import select  # SELECT query builder
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.models.item import ItemCreate, ItemResponse
-from app.db.memory_db import add_item, get_item_by_id
+from app.db.models import ItemModel
+from app.db.session import get_db
 
 router = APIRouter()
 
 @router.post("/items", response_model=ItemResponse, status_code=201)
-async def create_item(item: ItemCreate):
-    new_item = add_item(item)
-    return new_item
+async def create_item(item: ItemCreate, db: AsyncSession = Depends(get_db)):
+
+    db_item = ItemModel(**item.model_dump())
+    
+    db.add(db_item)
+    await db.commit()
+
+
+    await db.refresh(db_item)
+    
+    return db_item
 
 @router.get("/items/{item_id}", response_model=ItemResponse)
-async def read_item(item_id: int):
-    item = get_item_by_id(item_id)
-    if item is None:
+async def read_item(item_id: int, db: AsyncSession = Depends(get_db)):
+
+    result = await db.execute(select(ItemModel).where(ItemModel.id == item_id))
+    db_item = result.scalar_one_or_none() 
+    
+    if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    return item
+    
+    return db_item
